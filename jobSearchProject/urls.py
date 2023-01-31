4005e013a66bdcb6c821fb96d75ce124c3d5abb7
Zhavlon <zhavlon.khamidov@gmail.com>
2023-01-31 14:30:32 +0600

    introduced urls(not finished)

--- jobSearchProject/urls.py
+++ jobSearchProject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse("Hello World!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello', hello)
 ]
