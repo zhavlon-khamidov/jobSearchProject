40e0cd11a62202117cc14d5852c4c2a436dd467c
Zhavlon <zhavlon.khamidov@gmail.com>
2023-02-10 14:44:45 +0600

    Templates introduced

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from vacancies import views
 
 urlpatterns = [
-    path('',views.vacancies),
-    path('vacancy/<int:id>', views.vacancy)
+    path('',views.vacancies, name='vacancies'),
+    path('vacancy/<int:id>', views.vacancy, name='vacancy')
 ]
 
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+data = [
+    {'id':1, 'name':'Abai','surname': 'Tentimishov'},
+    {'id':2, 'name':'Azman','surname': 'Muratbekov'},
+    {'id':3, 'name':'Aktan','surname': 'Temirkanov'},
+    {'id':4, 'name':'Nurislam','surname': 'Orozbaev'},
+    {'id':5, 'name':'Sultan','surname': 'Sabatbekov'},
+]
 
 def vacancies(request):
-    return HttpResponse("List of all availabe vacancies")
+    # html_text = loader.render_to_string('vacancies.html')
+    # return HttpResponse(html_text)
+    return render(request,'vacancies.html',
+                  {'page':"Vacancies",'students':data})
 
 def vacancy(request,id):
-    return HttpResponse(f'Single vacancy with id: {id}')
+    # return HttpResponse(f'Single vacancy with id: {id}')
+    student = None
+    for s in data:
+        if s['id'] == id:
+            student = s
+            break
+    return render(request, 'single-vacancy.html',
+                  {'page':'Single-vacancy','student':student})
