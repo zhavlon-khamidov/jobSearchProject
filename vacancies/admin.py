d9cf19f90fdca61952ecc9d2c42b1eed31050d03
Zhavlon <zhavlon.khamidov@gmail.com>
2023-02-21 14:28:50 +0600

    1-Many & Many-Many relations on models

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Vacancy)
+admin.site.register(models.Company)
+
--- vacancies/models.py
+++ vacancies/models.py
@@ -6,13 +6,32 @@ class Vacancy(models.Model):
     description = models.TextField(blank=True, null=True)
     min_price = models.IntegerField(default=0)
     max_price = models.IntegerField(default=0)
-    # company
+    company = models.ForeignKey('Company',null=True,on_delete = models.CASCADE, default=None)
     city = models.CharField(blank=True, null=True,max_length=200)
-    # tags =
+    tags = models.ManyToManyField('Tag')
     
     created_date = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(primary_key=True,default=uuid.uuid4, unique=True,editable=False)
 
     
     def __str__(self) -> str:
-        return self.title
+        return self.title + ' (' + str(self.company) + ')'
+    
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=25, blank=True,null=True)
+    email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=255, blank=True,null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
