20158107d1057460788a00b4c3db36866cf1e02b
Zhavlon <zhavlon.khamidov@gmail.com>
2023-04-04 12:13:16 +0600

    Intro to signals

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from  django.core.signals import request_finished
+from django.db.models.signals import post_save
 
+def printInfo(**kwargs):
+    print(kwargs)
+    print("data saved")
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        post_save.connect(printInfo)
